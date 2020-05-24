7b1cbd937f1944ea19f966a5a5316a3ce259812c
nlukic97 <nlukic97@gmail.com>
2020-05-24 13:20:17 +0200

    Bug with initial checkbox (read p tag). Design in progress

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,16 +2,19 @@
   <div class="hello">
     <ul>
       <li>
-        <input type="checkbox" name="selectAll" id="selectAll">
+        <input type="checkbox" v-model="newTask.checked" name="newCheckbox" id="newCheckbox"> <!-- ovo checked nije skroz jasno -->
         <input type="text" v-bind:placeholder="placeholderValues.question" v-on:keyup.enter="addNewTask"  v-model="newTask.description" name="newTask" id="newTask">
         <button v-on:click="removeAllTasks">Clear List</button>
       </li>
-      <li v-for="(item, index) in tasks" v-bind:key="index"> <!--Nije najjasnije sve ovde, konkretno ovo bind -->
-        <input type="checkbox" v-on:click="removeTask(index)" name="clear" id="clear">
+      <li v-for="(item, index) in tasks" v-bind:key="index"> 
+        <input type="checkbox" v-model="item.checked" name="checkbox" id="checkbox"> <!-- K -->
         <span>{{item.description}}</span>
+        <!-- <span>{{item.checked}}</span> -->
         <button v-on:click="removeTask(index)">X</button>
       </li>
     </ul>
+    <p>Ako submitujemo bez clicka na prvi checkbox (uopste click), nastane problem.</p>
+    <p>Kad onda cekiramo i submitujemo, prikaze se kako treba ali se prvi checkbox ne vraca na unchecked.</p>
   </div>
 </template>
 
@@ -21,14 +24,17 @@ export default {
   data: function(){
     return {
       tasks:[{
-          description:'Do this and that'
+          description:'Task 2 - checked',
+          checked:true
         },
         {
-          description:'Do this again but for 2'
+          description:'Task 1 - not checked',
+          checked: false
         }
       ],
       newTask:[{
-        description:''
+        description:'',
+        checked:''
       }],
       placeholderValues:{
         question:'What do you need to do?'
@@ -37,10 +43,21 @@ export default {
   },
   methods:{
     addNewTask: function(){
+      //--------------------------------- ima li bolji nacin za ovo? -------------------------------
+      if(this.newTask.checked){
+        console.log('checked');
+        this.newTask.checked = true;
+      }   else {
+        console.log('Not checked');
+        this.newTask.checked = false ; //ovo stavljeno jer na prvi submit bez clicka na input, ne salje nista u newTask (nema false)
+      }
+      //---------------------------------------------------------------------------------------------
       this.tasks.unshift({
-        description: this.newTask.description
+        description: this.newTask.description,
+        checked: this.newTask.checked, //ne znam kako da napravim da checkbox bude checked ako je novi checked
       });
-      this.newTask.description = ''; //to clear input on enter
+      this.newTask.description = '',
+      this.newTask.checked = false //to reset the box back to normal
     },
     removeTask: function(taskIndex){ //Zasto se cekira odmah ispod opcija nakon sto prva nestane?
       this.tasks.splice(taskIndex, 1);
@@ -58,10 +75,40 @@ export default {
 .hello {
   border:1px solid red;
   ul{
+    margin:0;
+    padding:0;
     li{
       list-style-type: none;
       display:flex;
       justify-content: space-around;
+      align-items: center;
+      border:1px solid blue;
+      background-color: lightblue;
+      padding:15px 0;
+      &:first-child{
+        background-color:gray;
+      }
+      #newCheckbox, #checkbox {
+        width:20px;
+        height:20px;
+      }
+
+      #newTask{
+        border:1px solid #333;
+        width:65%;
+        padding:10px 10px 10px 10px;
+        font-size:14px;
+      }
+      span {
+        width:65%;
+        // border:1px solid #333;
+        width:65%;
+        padding:0 10px;
+        font-size:14px;
+      }
+      // span {
+      //   // text-decoration: line-through; //kako da dodajem klasu u vue methods
+      // }
     }
   }
 }
