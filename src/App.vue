861c0c85eecffb522fb9210fdc5b40906e99b219
nlukic97 <nlukic97@gmail.com>
2020-05-28 15:44:08 +0200

    changed bug description

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>To Do List</h1>
     <HelloWorld/>
-    <!-- <p>Ako submitujemo bez clicka na  <em><u>prvi checkbox (ako uopste ne kliknemo)</u></em>, nastane problem.</p>
+    <!-- <p>Ako submitujemo bez clicka na  <em><u>prvi checkbox (ako uopste ne kliknemo) i u input box nista ne ukucamo</u></em>, nastane problem.</p>
     <p>Kad nakon jednog submit-a kliknemo prvi input i submitujemo, prikaze se kako treba u listi ali se prvi checkbox ne vraca na unchecked sam to pokusao.</p> -->
   </div>
 </template>
